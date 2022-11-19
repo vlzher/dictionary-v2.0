681de09210a998c3b44728224f58fd58c40a1cca
Władysław <89138444+vlzher@users.noreply.github.com>
2022-11-19 16:23:57 +0100

    1.9

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import "./styles/app.scss"
 import RegistrationForm from "./components/RegistrationLogin/RegistrationForm";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import LoginForm from "./components/RegistrationLogin/LoginForm";
 import Dictionary from "./components/Dictionary";
-import StartForm from "./components/StartForm";
 function App() {
   return (
     <div className="app">
         <Routes>
             <Route path="/registration" element={<RegistrationForm/>}/>
-            <Route path="/" element={<StartForm/>}/>
+            {/*<Route path="/" element={<StartForm/>}/>*/}
             <Route path="/login" element={<LoginForm/>}/>
             <Route path="/words" element={<Dictionary/>}/>
-            {/*<Route path="*" element={<Navigate to={"/login"} replace/>}/>*/}
+            <Route path="*" element={<Navigate to={"/login"} replace/>}/>
         </Routes>
     </div>
   );
